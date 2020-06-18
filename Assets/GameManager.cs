b279231d3e1814abcc4bae79a37b0caefa9e2a93
Nohoon <leenh0726@naver.com>
2020-06-18 14:59:10 +0900

    Add attribute

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] image;
+    public Text scenarioText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //image = GameObject.Find("Image").GetComponent<Image>();
+       scenarioText = GameObject.Find("Scenario").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scenarioText.text = "널 팔아버리겠다고 했어";
+    }
+}
