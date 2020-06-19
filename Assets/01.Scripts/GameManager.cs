beb0697b58a0f1c9a352201b214ede2618f62933
Nohoon <leenh0726@naver.com>
2020-06-19 14:37:12 +0900

    이노훈
    UI 완성 및 Glass 연동 성공

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -8,9 +8,9 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public Sprite[] sprite;
-    public TextMeshProUGUI  scenarioText;
-    public Image image;
-    public GameObject uiCanvas;
+    TextMeshProUGUI  scenarioText;
+    Image Characters;
+    GameObject uiCanvas;
 
     private int count;
     List <string> scripts;
@@ -24,12 +24,12 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 0f;
         scripts = new List<string>();
-        image = GameObject.Find("Image").GetComponent<Image>();
+        Characters = GameObject.Find("Characters").GetComponent<Image>();
         uiCanvas = GameObject.Find("UICanvas");
         scenarioText = GameObject.Find("Scenario").GetComponent<TextMeshProUGUI>();
 
         //scenarioText.text = "널 팔아버리겠다고 했어";
-        image.sprite = sprite[0];
+        Characters.sprite = sprite[0];
         count = 0;
 
         SetScript();
@@ -37,10 +37,14 @@ public class GameManager : MonoBehaviour
 
     private void SetScript()
     {
+        scripts.Add("어느 시골 농장에 늙은 당나귀 한마리가 있었어요.");
+        scripts.Add("당나귀는 주인을 위해 이른 아침부터 밤늦게까지 열심히 일했습니다");
+        scripts.Add("하지만 당나귀 주인은 늙은 당나귀는 더이상 쓸모없다며 시장에 내다 팔려고 했습니다");
+        scripts.Add("이소식을 들은 새 한마리가 급하게 당나귀에게 알려주러 갔습니다" );
         scripts.Add("널 팔아버리겠다고 했어");
-        scripts.Add("!!!");
+        scripts.Add("호에에에엥");
         scripts.Add("도망치자");
-        scripts.Add("그랭그랭");
+        scripts.Add("그랭그랭(아무키나 입력하면 게임이 시작됩니다)");
 
 
     }
@@ -51,29 +55,71 @@ public class GameManager : MonoBehaviour
         if(Input.anyKeyDown)
             ++count;
 
-        if(3<=count)
-         count = 3;
+        if(8<=count)
+         count = 8;
         
 
-        ChangeUI(count);
+        ChangeUI();
 
 
 
     }
 
-    private void ChangeUI(int _count)
+    private void ChangeUI()
     {
-        if(3 >_count)
-        {
-            int arr = _count %2;
-            image.sprite = sprite[arr];
-            scenarioText.text = scripts[_count];
 
-        }
-        else
+        switch(count)
         {
+            default:break;
+
+            case 0:
+                Characters.sprite = sprite[0];
+                scenarioText.text = scripts[count];
+
+            break;
+
+            case 1:
+                Characters.sprite = sprite[0];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 2:
+                Characters.sprite = sprite[0];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 3:
+                Characters.sprite = sprite[0];
+                scenarioText.text = scripts[count];
+            break;
+
+             case 4:
+                Characters.sprite = sprite[1];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 5:
+                Characters.sprite = sprite[2];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 6:
+                Characters.sprite = sprite[1];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 7:
+                Characters.sprite = sprite[2];
+                scenarioText.text = scripts[count];
+            break;
+
+            case 8: 
             Time.timeScale = 1f;
             uiCanvas.SetActive(false);
+            break;
+
+            
+
         }
     }
 }
